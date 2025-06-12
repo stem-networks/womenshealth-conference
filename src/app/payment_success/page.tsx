d79de61705b689268cf746deceb8bf874596f2ac
stem-networks <stem.networks.india@gmail.com>
2025-06-12 12:42:35 +0530

    canonical updated and Forms changes -Srilakshmi

--- src/app/payment_success/page.tsx
+++ src/app/payment_success/page.tsx
@@ -1,6 +1,40 @@
 // File: app/payment_success/page.tsx
 import PaymentSuccess from "../components/PaymentSuccess";
 import { ApiResponse } from "@/types";
+import { Metadata } from "next";
+
+export async function generateMetadata(): Promise<Metadata> {
+    try {
+        // const generalData = await fetchGeneralData();
+        // const meta = generalData?.pages?.sessions_meta?.[0] || {
+        //     title: "Sessions",
+        //     content: "Explore the sessions of the conference.",
+        //     meta_keywords: "",
+        // };
+
+        // Canonical 
+        const baseUrl = process.env.BASE_URL || '';
+        const canonicalPath = '/payment_success'; // hardcode since we know this is sessions page
+        const canonicalURL = `${baseUrl}${canonicalPath}`;
+
+        return {
+            // title: meta.title,
+            // description: meta.content,
+            // keywords: meta.meta_keywords,
+            metadataBase: new URL(baseUrl),
+            alternates: {
+                canonical: canonicalURL,
+            },
+        };
+    } catch (error) {
+        console.error("Metadata generation error sessions:", error);
+        return {
+            title: "Register Details",
+            description: "Explore the Register of the conference.",
+            keywords: "",
+        };
+    }
+}
 
 async function fetchGeneralData(): Promise<ApiResponse> {
   const baseUrl = process.env.BASE_URL;
