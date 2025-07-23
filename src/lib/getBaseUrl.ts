20d6584b09b5817efb631f79220e09dd8bb355ff
stem-networks <stem.networks.india@gmail.com>
2025-07-23 14:28:50 +0530

    - vijay

--- src/lib/getBaseUrl.ts
+++ src/lib/getBaseUrl.ts
@@ -5,11 +5,11 @@ export function getBaseUrl() {
     // Always use relative on client
     return "";
   }
-  // if (process.env.VERCEL_URL) {
-  //   return `https://${process.env.VERCEL_URL}`;
-  // }
-  if (process.env.NEXT_PUBLIC_BASE_URL) {
-    return process.env.NEXT_PUBLIC_BASE_URL;
+  if (process.env.VERCEL_URL) {
+    return `https://${process.env.VERCEL_URL}`;
+  }
+  if (process.env.BASE_URL) {
+    return process.env.BASE_URL;
   }
   return "http://localhost:3000";
 }
