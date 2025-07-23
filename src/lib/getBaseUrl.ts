26cafa0c420b6f1d9d63fbac44294692b07999d8
stem-networks <stem.networks.india@gmail.com>
2025-07-23 12:37:55 +0530

    new file setup666

--- src/lib/getBaseUrl.ts
+++ src/lib/getBaseUrl.ts
@@ -8,8 +8,8 @@ export function getBaseUrl() {
   // if (process.env.VERCEL_URL) {
   //   return `https://${process.env.VERCEL_URL}`;
   // }
-  if (process.env.BASE_URL) {
-    return process.env.BASE_URL;
+  if (process.env.NEXT_PUBLIC_BASE_URL) {
+    return process.env.NEXT_PUBLIC_BASE_URL;
   }
   return "http://localhost:3000";
 }
